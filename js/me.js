af29fcbe263f09b8a0307ed9eb1b9b3213856944
Elvisthacoder <ekk213@gmail.com>
2016-04-05 14:36:19 +0300

    refresher

---
+++ js/me.js
@@ -0,0 +1,11 @@
+/* =================================
+   TYPING EFFECT
+=================================== */
+(function($) {
+    "use strict";
+
+    $('[data-typer-targets]').typer();
+    $.typer.options.clearOnHighlight=false;
+
+})(jQuery);
+
